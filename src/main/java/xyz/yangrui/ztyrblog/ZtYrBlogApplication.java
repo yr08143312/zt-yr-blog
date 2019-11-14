0f176e69aa2f49bbb4cd124a97d54ea432ac42d3
yr08143312 <yr08143312@163.com>
2019-11-14 15:45:07 +0800

    首次提交

---
+++ src/main/java/xyz/yangrui/ztyrblog/ZtYrBlogApplication.java
@@ -0,0 +1,13 @@
+package xyz.yangrui.ztyrblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtYrBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtYrBlogApplication.class, args);
+    }
+
+}
