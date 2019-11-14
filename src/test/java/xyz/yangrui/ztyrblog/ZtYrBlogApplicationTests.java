c9cc484f01da32134acb4ba2d2a6e910f95a67ac
yr08143312 <yr08143312@163.com>
2019-11-14 15:51:52 +0800

    首次完整提交

---
+++ src/test/java/xyz/yangrui/ztyrblog/ZtYrBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.yangrui.ztyrblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtYrBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
